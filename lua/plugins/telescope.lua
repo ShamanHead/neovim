7d5bdc4b8e7e3834961aa40bc27d36719321ca8d
Arseniy Romanovskiy <ronyplay247@gmail.com>
2023-06-12 04:33:00 +0300

    Remove little error

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,8 +2,7 @@ return {
     {
         'nvim-telescope/telescope.nvim',
         dependencies = {
-            'nvim-lua/plenary.nvim',
-            ''
+            'nvim-lua/plenary.nvim'
         },
         config = function()
             require("telescope").setup {
